e7885dd2b577979706193cc314d0871da85fc720
andz2 <Andrey.Zeml@gmail.com>
2014-09-21 17:10:10 +0600

    first

---
+++ app/src/main/java/ru/xxmmk/skdmobile/MapsActivity.java
@@ -0,0 +1,98 @@
+package ru.xxmmk.skdmobile;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+    LocationManager locationManager;
+    Criteria criteria = new Criteria();
+
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        // Get the LocationManager object from the System Service LOCATION_SERVICE
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+           }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+    //  Log.d( locationManager.getBestProvider(criteria, true),"criteria");
+      //  String provider = locationManager.getBestProvider(criteria, true);
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+               setUpMap();
+           //     drawMarker(locationManager.getLastKnownLocation(provider));
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+   // рисуем маркер по координате
+   private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+
+    private void drawMarker(Location location){
+        mMap.clear();
+
+//  convert the location object to a LatLng object that can be used by the map API
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+// zoom to the current location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 16));
+
+// add a marker to the map indicating our current position
+        mMap.addMarker(new MarkerOptions()
+                .position(currentPosition)
+                .snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude()));
+    }
+}
